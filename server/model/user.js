9e544d2a35e463d90500d9a3046242d55f1400b9
hasanfqq <hasansyed.fahad@ontariotechu.net>
2024-11-28 20:51:59 -0500

    Update user.js
    completion of user.js

--- server/model/user.js
+++ server/model/user.js
@@ -5,3 +5,12 @@ const userSchema = new mongoose.Schema({
 
     email: {type: String, required: true , unique: true}, password: {type: String , required: true}
 });
+
+userSchema.pre('save' , async function (next){
+
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+module.exports = mongoose.model('user' , userSchema);
