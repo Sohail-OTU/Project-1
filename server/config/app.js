f72e424f3327dea050511fee1aa021adfc9655b7
Sohail-OTU <sohail.baig@ontariotechu.net>
2024-12-05 23:44:29 -0500

    summaty

--- server/config/app.js
+++ server/config/app.js
@@ -14,8 +14,8 @@ let checkAuthentication = require('../../middleware/auth')
 
 
 // view engine setup
-app.set('views', path.join(__dirname, '../views'));
-app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, '../views'));
+// app.set('view engine', 'ejs');
 // getting-started.js
 const mongoose = require('mongoose');
 mongoose
